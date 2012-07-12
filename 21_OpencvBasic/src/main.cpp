204697e213dd5f4d8812a879ec71ff2d6b473d43
horaotoko <horaotoko@gmail.com>
2012-07-12 17:05:15 +0900

    second commit

--- 21_OpencvBasic/src/main.cpp
+++ 21_OpencvBasic/src/main.cpp
@@ -6,8 +6,8 @@
 int main( ){
 
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
-
+	//ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, 320,240, OF_WINDOW);
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
